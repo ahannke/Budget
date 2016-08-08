5137e2c694857675d9c86ec2169941e04028aa9d
ahannke <ahannke@gmail.com>
2016-08-08 08:16:07 +0200

    metody przeniesione do PaniZBiuraPodrozy, ale dailyBudget niedostepny w Budget

--- src/main/java/it/morfoza/Budget.java
+++ src/main/java/it/morfoza/Budget.java
@@ -22,41 +22,34 @@ public class Budget {
         PaniZBiuraPodrozy milaPaniZBiura = new PaniZBiuraPodrozy(allDestinations);
 
         milaPaniZBiura.zrobWywiad();
-
-      //  showAffordableDestinations(dailyBudget);
-
-    }
-
-    public static void showAffordableDestinations(double dailyBudget) {
-        List<Destination> allDestinations = new ArrayList<>();
-        allDestinations.add(new Destination("Tajlandia", 215, 6500));
-        allDestinations.add(new Destination("Białoruś", 59, 800));
-        allDestinations.add(new Destination("Kanary", 249, 2800));
-
-        List<Destination> affordableDestinations = getAffordableDestinations(allDestinations, dailyBudget);
-        System.out.println("Oto lista wszystkich  destynacji: " + allDestinations);
-        System.out.println("Oto lista destynacji na ktore Cię stac:" + affordableDestinations);
-    }
-
-    public static List<Destination> getAffordableDestinations(List<Destination> destinations, double dailyBudget){
-
-       List<Destination> affordableDestinations = new LinkedList<>();
-
-//       affordableDestinations.add(new Destination("Tajlandia", 215, 6500));
-//       affordableDestinations.add(new Destination("Białoruś", 59, 800));
-//       affordableDestinations.add(new Destination("Kanary", 249, 2800));
-
-        for (Destination destination : destinations){
-            if (destination.pricePerDay<=dailyBudget) {
-                affordableDestinations.add(destination);
-            }
-        }
-
-        return affordableDestinations;
-
+        milaPaniZBiura.showAffordableDestinations(double dailyBudget);
 
 
     }
+
+//    public static void showAffordableDestinations(double dailyBudget) {
+//        List<Destination> allDestinations = new ArrayList<>();
+//        allDestinations.add(new Destination("Tajlandia", 215, 6500));
+//        allDestinations.add(new Destination("Białoruś", 59, 800));
+//        allDestinations.add(new Destination("Kanary", 249, 2800));
+//
+//        List<Destination> affordableDestinations = getAffordableDestinations(allDestinations, dailyBudget);
+//        System.out.println("Oto lista wszystkich  destynacji: " + allDestinations);
+//        System.out.println("Oto lista destynacji na ktore Cię stac:" + affordableDestinations);
+//    }
+//
+//    public static List<Destination> getAffordableDestinations(List<Destination> destinations, double dailyBudget){
+//
+//       List<Destination> affordableDestinations = new LinkedList<>();
+//        for (Destination destination : destinations){
+//            if (destination.pricePerDay<=dailyBudget) {
+//                affordableDestinations.add(destination);
+//            }
+//        }
+//
+//        return affordableDestinations;
+//
+//    }
 
 
 
--- src/main/java/it/morfoza/PaniZBiuraPodrozy.java
+++ src/main/java/it/morfoza/PaniZBiuraPodrozy.java
@@ -1,7 +1,11 @@
 package it.morfoza;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+
+
 
 /**
  * Created by ahann on 02/08/2016.
@@ -10,6 +14,9 @@ public class PaniZBiuraPodrozy {
 
     private String name = "Zuzia";
     private List<Destination> allDestinations;
+
+    double number1;
+    double number2;
 
     public PaniZBiuraPodrozy(List<Destination> allDestinations) {
         this.allDestinations = allDestinations;
@@ -35,11 +42,33 @@ public class PaniZBiuraPodrozy {
         double dailyBudget = number1 / number2;
         System.out.println("Twój dzienny budżet to: " + dailyBudget);
 
-        // sprawdz ktna ore oferty z listy stac klienta
+        // sprawdz na ktore oferty z listy stac klienta
 
         // pokaz te oferty
 
+    }
 
+    public void showAffordableDestinations(double dailyBudget) {
+        List<Destination> allDestinations = new ArrayList<>();
+        allDestinations.add(new Destination("Tajlandia", 215, 6500));
+        allDestinations.add(new Destination("Białoruś", 59, 800));
+        allDestinations.add(new Destination("Kanary", 249, 2800));
+
+        List<Destination> affordableDestinations = getAffordableDestinations(allDestinations, dailyBudget);
+        System.out.println("Oto lista wszystkich  destynacji: " + allDestinations);
+        System.out.println("Oto lista destynacji na ktore Cię stac:" + affordableDestinations);
+    }
+
+    public List<Destination> getAffordableDestinations(List<Destination> destinations, double dailyBudget) {
+
+        List<Destination> affordableDestinations = new LinkedList<>();
+        for (Destination destination : destinations) {
+            if (destination.pricePerDay <= dailyBudget) {
+                affordableDestinations.add(destination);
+            }
+        }
+
+        return affordableDestinations;
     }
 
 }
