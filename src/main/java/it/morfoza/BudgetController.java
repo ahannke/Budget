86e60a980233c6a9ff5d0e0a734f900860570567
ahannke <ahannke@gmail.com>
2016-08-10 14:39:49 +0200

    dailyBudget counted correctly on BudgetController result.html renamed to dailyBudget.html affordableDestinations added to dailyBudget.html

--- src/main/java/it/morfoza/BudgetController.java
+++ src/main/java/it/morfoza/BudgetController.java
@@ -1,8 +1,9 @@
 package it.morfoza;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -10,8 +11,17 @@ public class BudgetController {
 
     @RequestMapping("/wywiad")
     public String wywiad() {
-
         return "wywiad";
+    }
+
+    @RequestMapping("/dailyBudget")
+    public String dailyBudget(@RequestParam(value = "number1", defaultValue = "0") Integer number1,
+                              @RequestParam(value = "number2", defaultValue = "1") Integer number2,
+                              @RequestParam(value = "affordableDestinations", required = false)
+                                          String showDailyBudget, Model model) {
+        model.addAttribute("dailyBudget", number1 / number2);
+
+        return "dailyBudget";
     }
 
 
